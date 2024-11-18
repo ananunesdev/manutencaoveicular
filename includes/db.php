92832d8e5ed1ff834ebd512bad6f0ec377389bfb
Ana Nunes <ananunes.dev@gmail.com>
2024-11-18 20:04:38 -0300

    create db.php
    criação de conexão com banco de dados

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php 
+    //declarar variável
+    $host = 'localhost';
+    $dbname = 'sistema_manutencao';
+    $username = 'root';
+    $password = '';
+
+   try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   } catch (PDOException $e) {
+        die("Erro ao conectar ao banco de dados:" . $e -> getMessage());
+   }
+
+?>
