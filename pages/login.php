e6e0afe571c6ef2bfefa3801c897bf7f33c0add3
Ana Nunes <ananunes.dev@gmail.com>
2024-11-21 20:05:41 -0300

    validacao de usuario e senha e inclusao de header

--- pages/login.php
+++ pages/login.php
@@ -5,14 +5,39 @@
 
     $error = '';
 
-    //verificar se formulário foi enviado
+    //consultar banco de dados para encontrar o usuario
+
+    $stmt = $pdo -> prepare("select * from users where email = ?;");
+    $stmt -> execute([$email]);
+    $user = $stmt->fetch();
 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $email = trim($_POST['$email']);
         $password = trim($_POST['$password']);
 
+        if(empty($email) || empty($password)){
+            $error = "Todos os campos são obrigatórios.";
+        }else{
+            //verifica se o usuário existe e a senha está correta
+
+            if($user && password_verify($password, $user['password'])){
+            //salvar o ID do usuário na sessão e redirecionar para o dashboard
+            //redirecionar para o dashboard
+           
+                $_SESSION['user_id'] = $user['id'];
+
+                echo $_SESSION['user_id'];
+                header('refresh:5; url= dashboard.php');
+                exit();
+    
+            }else{
+                $error = "E-mail ou senha inválidos.";
+             }
+        }
+
         
     }
+    include '../includes/header.php';
 
 ?>
 
