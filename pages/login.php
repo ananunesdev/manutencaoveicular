242766c5f244e50c122823f1a11d219cf2e70e3f
Ana Nunes <ananunes.dev@gmail.com>
2024-11-21 21:05:55 -0300

    pagina de login e validação

--- pages/login.php
+++ pages/login.php
@@ -5,15 +5,14 @@
 
     $error = '';
 
-    //consultar banco de dados para encontrar o usuario
-
-    $stmt = $pdo -> prepare("select * from users where email = ?;");
-    $stmt -> execute([$email]);
-    $user = $stmt->fetch();
-
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $email = trim($_POST['$email']);
         $password = trim($_POST['$password']);
+
+        //consultar banco de dados para encontrar o usuario
+        $stmt = $pdo -> prepare("select * from users where email = ?;");
+        $stmt -> execute([$email]);
+        $user = $stmt->fetch();
 
         if(empty($email) || empty($password)){
             $error = "Todos os campos são obrigatórios.";
@@ -41,14 +40,28 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h2 class="text-center">Login</h2>
+
+            <?php if(!empty($erro)):?>
+                <div class="alert alert-danger"><?= $error?></div>
+            <?php endif; ?>
+            <form method="POST">
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" required placeholder="Ex.: seu@email.com">
+                </div>
+                <div class="mb-3">
+                <label for="password" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="password" name="password"required>
+                </div>
+                <button type="submit" class="btn btn-dark w-100">Entrar</button>
+            </form>
+            <p class="text-center mt-3">Não tem uma conta? <a href="register.php">Registre-se</a> </p>
+        </div>
+    </div>
+
+</div>
+<?php include'../includes/footer.php';?>
